2b85dce1fddba1af1022717be167b9460c9b2b54
maroeca <maycow.carmo@gmail.com>
2017-04-05 14:45:13 -0300

    colisoes ok

--- Exercicios/Exercicio03/Source.cpp
+++ Exercicios/Exercicio03/Source.cpp
@@ -1,153 +1,56 @@
 #include <iostream>
-#include "hash.h"
-#include <string>
-#include <cstdlib>
+#include <stdlib.h>
 
-using namespace std;
+#define TAM 1000
+#define KEYS 10000
 
-int main(int argc, char *argv[])
+void zeraVetor(int v[]) //inicia os vetores com 0
 {
-	hash ObjetoHash;
+	for (int i = 0; i < TAM; i++)
+	{
+		v[i] = 0;
+	}
+}
 
-	ObjetoHash.Inserir("auto");
-	ObjetoHash.Inserir("asm");
-	ObjetoHash.Inserir("bool");
-	ObjetoHash.Inserir("break");
-	ObjetoHash.Inserir("case");
-	ObjetoHash.Inserir("catch");
-	ObjetoHash.Inserir("char");
-	ObjetoHash.Inserir("class");
-	ObjetoHash.Inserir("const");
-	ObjetoHash.Inserir("const_cast");
-	ObjetoHash.Inserir("continue");
-	ObjetoHash.Inserir("default");
-	ObjetoHash.Inserir("delete");
-	ObjetoHash.Inserir("do");
-	ObjetoHash.Inserir("double");
-	ObjetoHash.Inserir("dynamic_cast");
-	ObjetoHash.Inserir("else");
-	ObjetoHash.Inserir("enum");
-	ObjetoHash.Inserir("explicit");
-	ObjetoHash.Inserir("extern");
-	ObjetoHash.Inserir("false");
-	ObjetoHash.Inserir("float");
-	ObjetoHash.Inserir("for");
-	ObjetoHash.Inserir("friend");
-	ObjetoHash.Inserir("goto");
-	ObjetoHash.Inserir("if");
-	ObjetoHash.Inserir("inline");
-	ObjetoHash.Inserir("int");
-	ObjetoHash.Inserir("long");
-	ObjetoHash.Inserir("mutable");
-	ObjetoHash.Inserir("namespace");
-	ObjetoHash.Inserir("new");
-	ObjetoHash.Inserir("operator");
-	ObjetoHash.Inserir("private");
-	ObjetoHash.Inserir("protected");
-	ObjetoHash.Inserir("public");
-	ObjetoHash.Inserir("register");
-	ObjetoHash.Inserir("reinterpret_cast");
-	ObjetoHash.Inserir("return");
-	ObjetoHash.Inserir("short");
-	ObjetoHash.Inserir("signed");
-	ObjetoHash.Inserir("sizeof");
-	ObjetoHash.Inserir("static");
-	ObjetoHash.Inserir("static_cast");
-	ObjetoHash.Inserir("struct");
-	ObjetoHash.Inserir("switch");
-	ObjetoHash.Inserir("template");
-	ObjetoHash.Inserir("this");
-	ObjetoHash.Inserir("throw");
-	ObjetoHash.Inserir("true");
-	ObjetoHash.Inserir("try");
-	ObjetoHash.Inserir("typedef");
-	ObjetoHash.Inserir("typeid");
-	ObjetoHash.Inserir("typename");
-	ObjetoHash.Inserir("union");
-	ObjetoHash.Inserir("unsigned");
-	ObjetoHash.Inserir("using");
-	ObjetoHash.Inserir("virtual");
-	ObjetoHash.Inserir("void");
-	ObjetoHash.Inserir("volatile");
-	ObjetoHash.Inserir("wchar_t");
-	ObjetoHash.Inserir("while");
+void testaColisão(int v[]) //testa as colisoes nas posicoes
+{
+	for (int i = 0; i < KEYS; i++)
+	{
+		int h = i%TAM;
+		v[h]++;
+	}
+}
+//funciona git 
+int main()
+{
+	int v[TAM];
+	FILE* fp;
 
-	int opcao = 0;
-	int aux;
-	string palavra = "";
+	char arq[] = "Colisoes.txt"; //string com o nome do arquivo e a extenção
 
-	while (opcao != 4) {
-		cout << "      **** MENU ****" << endl << endl;
-		cout << "Total de palavras reservadas = 62\nEspalhadas em uma tabela hash de tamanho 10\n\n";
-		cout << "1 - Buscar uma palavra reservada" << endl;
-		cout << "2 - Imprimir tabela por indice" << endl;
-		cout << "3 - Imprimir toda a tabela" << endl;
-		cout << "4 - Para finalizar" << endl;
-
-		cout << endl << endl << "Digite sua opcao: " << endl;
-		cin >> opcao;
-		switch (opcao) {
-		case 1:
-			while (palavra != "sair") {
-				cout << "Para terminar a busca, digite sair\nBuscar por: " << endl;
-				cin >> palavra;
-				if (palavra != "sair") {
-					ObjetoHash.BuscaPalavra(palavra);
-				}
-			}
-			break;
-		case 2:
-			cout << endl << "Digite o indice que deseja imprimir\nOBS: de 0 a 9" << endl;
-			cin >> aux;
-			if (aux == 0) {
-				ObjetoHash.ImprimirItensNoIndice(0);
-				cout << endl;
-			}
-			if (aux == 1) {
-				ObjetoHash.ImprimirItensNoIndice(1);
-				cout << endl;
-			}
-			if (aux == 2) {
-				ObjetoHash.ImprimirItensNoIndice(2);
-				cout << endl;
-			}
-			if (aux == 3) {
-				ObjetoHash.ImprimirItensNoIndice(3);
-				cout << endl;
-			}
-			if (aux == 4) {
-				ObjetoHash.ImprimirItensNoIndice(4);
-				cout << endl;
-			}
-			if (aux == 5) {
-				ObjetoHash.ImprimirItensNoIndice(5);
-				cout << endl;
-			}
-			if (aux == 6) {
-				ObjetoHash.ImprimirItensNoIndice(6);
-				cout << endl;
-			}
-			if (aux == 7) {
-				ObjetoHash.ImprimirItensNoIndice(7);
-				cout << endl;
-			}
-			if (aux == 8) {
-				ObjetoHash.ImprimirItensNoIndice(8);
-				cout << endl;
-			}
-			if (aux == 9) {
-				ObjetoHash.ImprimirItensNoIndice(9);
-				cout << endl;
-			}
-			break;
-		case 3:
-			cout << endl;
-			ObjetoHash.ImprimirTabela();
-			break;
-		default:
-			cout << "Escolha invalida!" << endl << endl;
-			break;
-		}
+	if ((fopen_s(&fp, arq, "a")) == 0) //fopen_s pra abrir o arquivo e ja verifica se deu certo "a" é o tipo de operação(ler,gravar...)
+	{
+		std::cout << "O arquivo foi aberto!" << std::endl;
+	}
+	else
+	{
+		std::cout << "O arquivo nao pode ser aberto!" << std::endl;
 		return 0;
 	}
+	zeraVetor(v);
+	testaColisão(v);
+
+	for (int i = 0; i < TAM; i++)
+	{
+		fprintf(fp, "%d\n", v[i]);
+	}
+
+	fclose(fp);
+
+
+
+
+
+	system("PAUSE");
+	return 0;
 }
